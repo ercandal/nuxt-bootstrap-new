a40f54dddfb321925f94927eccc90d87629016d4
ercandal <ercandall@hotmail.com>
2024-08-12 22:45:03 +0300

    initialize repository

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button class="btn btn-primary">
+            <NuxtLink to="/">Home</NuxtLink>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button class="btn btn-primary">
+            <NuxtLink to="/about">About</NuxtLink>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
